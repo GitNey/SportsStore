4dcb66cb510bf5111a6799e923249e8e42851b79
Ney Hernandez <nher1625@system76-pc.localdomain>
2020-05-20 01:06:11 -0400

    inital commit.

---
+++ data.js
@@ -0,0 +1,13 @@
+/*
+    The sportstore app will use async http requests to get model data
+    provided by a RESTful web service.
+
+    REST is an approach to building web services that uses the http method
+    or verb to specifiy an operation and a URL to select the data objects
+    that the operations apply to.
+
+    The json-webserver package is a tool for quickly generating web services from JSON
+    data.
+
+    This ensures a fixed state that the project can be reset to.
+*/
