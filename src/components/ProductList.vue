b55e7945efa4f798987ebb7b4d4e2e5b0852e0ca
Ney Hernandez <nher1625@gmail.com>
2020-05-20 21:29:53 -0400

    Define the product list component and note the use of mapState operation from vuex in this component to define computed property from store state property...

---
+++ src/components/ProductList.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        {{
+            /**
+                Keys are important way of defining data fragments on the dom 
+                for efficient response to data manipulation (v-bind:key="p.id")
+             */
+        }}
+        <div 
+            v-for="p in products" 
+            v-bind:key="p.id"
+            class="card bg-light m-1 p-1">
+            <h4>
+                {{ p.name }}
+                <span class="badge badge-pill badge-primary float-right">
+                    {{ p.price }}
+                </span>
+            </h4>
+            <div class="card-text bg-white p-1">{{ p.description }}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapState } from 'vuex' // impport the mapState operation from vuex
+export default {
+    computed: {
+        ...mapState(['products']) // single expression declaration of computed 
+        /**
+         * Shorthand from
+         * computed: {
+         *  products() {
+         *    return Store.state.products
+         *  }
+         * }
+         */
+    }
+}
+</script>
